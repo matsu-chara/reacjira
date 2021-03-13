63b3dd213a81802b39d87cf1936eb4d14346f9dc
matsu-chara <matsuy00@gmail.com>
2021-03-13 13:33:11 +0900

    add message handler

---
+++ bot/bot.go
@@ -0,0 +1,50 @@
+package bot
+
+import (
+	"log"
+
+	"reacjira/bot/handler"
+	"reacjira/config"
+
+	"github.com/slack-go/slack"
+)
+
+type Bot struct {
+	commandHandler *handler.CommandHandler
+}
+
+// Run activate bot routine
+func Run(api *slack.Client, slackCtx config.SlackCtx, jiraCtx config.JiraCtx, reacjiras []config.Reacjira) int {
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	// initialized when connected
+	var bot *Bot
+
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			log.Print("hello")
+		case *slack.ConnectedEvent:
+			log.Print("connected")
+			bot = &Bot{
+				commandHandler: handler.NewCommandHandler(
+					rtm,
+					slackCtx,
+					jiraCtx,
+					config.Profile{
+						ID:   ev.Info.User.ID,
+						Name: ev.Info.User.Name,
+					},
+					reacjiras,
+				),
+			}
+		case *slack.InvalidAuthEvent:
+			log.Printf("Invalid credentials. %v", ev)
+			return 1
+		case *slack.ReactionAddedEvent:
+			bot.commandHandler.HandleCommand(ev)
+		}
+	}
+	return 1
+}
---
+++ bot/handler/handler.go
@@ -0,0 +1,153 @@
+package handler
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"strings"
+
+	"github.com/slack-go/slack"
+
+	"reacjira/config"
+	myjira "reacjira/jira"
+	myslack "reacjira/slack"
+)
+
+type CommandHandler struct {
+	slackMessenger *myslack.Messenger
+	jira           *myjira.MyJira
+	slackCtx       config.SlackCtx
+	botProfile     config.Profile
+	reacjiras      []config.Reacjira
+}
+
+func NewCommandHandler(
+	rtm *slack.RTM,
+	slackCtx config.SlackCtx,
+	jiraCtx config.JiraCtx,
+	botProfile config.Profile,
+	reacjiras []config.Reacjira,
+) *CommandHandler {
+	return &CommandHandler{
+		myslack.New(rtm),
+		myjira.New(jiraCtx.Host, jiraCtx.Email, jiraCtx.Token),
+		slackCtx,
+		botProfile,
+		reacjiras,
+	}
+}
+
+func (commandHandler *CommandHandler) HandleCommand(ev *slack.ReactionAddedEvent) {
+	// search reacjira
+	var found config.Reacjira
+
+	for _, r := range commandHandler.reacjiras {
+		if r.Emoji == ev.Reaction {
+			found = r
+		}
+	}
+
+	if found.Emoji == "" {
+		// if reacjira was not found, then ignore this command
+		return
+	}
+
+	commandHandler.createTicket(ev, found)
+}
+
+func (commandHandler *CommandHandler) createTicket(ev *slack.ReactionAddedEvent, reacjira config.Reacjira) {
+	log.Printf("A slack reaction was received: channel: %s, timestamp: %s, user: %s", ev.Item.Channel, ev.Item.Timestamp, ev.User)
+	log.Printf("prepare to create a ticket %+v", reacjira)
+
+	// fetch slack message
+	msg, err := findMessage(commandHandler.slackMessenger, ev)
+	if err != nil {
+		log.Printf("findMessage error: %s", err.Error())
+		if msg != nil {
+
+			commandHandler.slackMessenger.SendMessages([]string{err.Error()}, ev.Item.Channel, msg.ThreadTimestamp)
+		}
+		return
+	}
+	var replyTo = msg.ThreadTimestamp
+	if msg.ThreadTimestamp == "" {
+		replyTo = msg.Timestamp
+	}
+
+	for _, reaction := range msg.Reactions {
+		if reaction.Name == reacjira.Emoji && reaction.Count > 1 {
+			log.Printf("multiple reacjira reactions were found(%s, %d). skip", reaction.Name, reaction.Count)
+			return
+		}
+	}
+
+	reporter, err := commandHandler.slackMessenger.SearchUser(msg.User)
+	if err != nil {
+		log.Printf("searchUser error: %s", err.Error())
+		commandHandler.slackMessenger.SendMessages([]string{err.Error()}, ev.Item.Channel, replyTo)
+		return
+	}
+
+	channel, err := commandHandler.slackMessenger.SearchChannel(ev.Item.Channel)
+	if err != nil {
+		log.Printf("searchChannel error: %s", err.Error())
+		commandHandler.slackMessenger.SendMessages([]string{err.Error()}, ev.Item.Channel, replyTo)
+		return
+	}
+
+	link, err := getPermLink(commandHandler.slackMessenger, ev, msg)
+	if err != nil {
+		log.Printf("getPermLink error: %s", err.Error())
+		commandHandler.slackMessenger.SendMessages([]string{err.Error()}, ev.Item.Channel, replyTo)
+		return
+	}
+
+	title := strings.Replace(msg.Text, "\n", " ", -1)
+	limit := int(math.Min(float64(len(title)), 200))
+	title = title[0:limit]
+
+	description := fmt.Sprintf(`auto generated
+from: %s
+at: %s
+%s`, link, channel.Name, reacjira.Description)
+	log.Printf("reporter:%s, channel: %s, title: %s", reporter.Name, channel.Name, title)
+
+	log.Printf("attempt to create a ticket.")
+	ticket, err := commandHandler.jira.CreateTicket(
+		reacjira.Project,
+		reacjira.IssueType,
+		reacjira.EpicKey,
+		reporter.Profile.Email,
+		title,
+		description,
+	)
+	if err != nil {
+		log.Printf("createTicket error: %s", err.Error())
+		commandHandler.slackMessenger.SendMessages([]string{err.Error()}, ev.Item.Channel, replyTo)
+		return
+	}
+
+	commandHandler.slackMessenger.SendMessages([]string{*ticket}, ev.Item.Channel, replyTo)
+}
+
+func findMessage(slackMessenger *myslack.Messenger, ev *slack.ReactionAddedEvent) (*slack.Message, error) {
+	msg, err := slackMessenger.SearchMsg(ev.Item.Channel, ev.Item.Timestamp)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	log.Printf("slack message search finish: %s, %s, %s, reply:%d", msg.User, msg.Text, msg.Timestamp, msg.ReplyCount)
+	return msg, nil
+}
+
+func getPermLink(slackMessenger *myslack.Messenger, ev *slack.ReactionAddedEvent, msg *slack.Message) (string, error) {
+	link, err := slackMessenger.GetPermLink(ev.Item.Channel, msg.Timestamp)
+	if err != nil {
+		log.Println(err.Error())
+		return "", err
+	}
+
+	log.Printf("got slack permlink: %s", link)
+	return link, nil
+}
